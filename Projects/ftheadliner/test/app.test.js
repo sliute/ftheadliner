86924e118e3d3e99a576f5d1eb320ecf9a87bf5f
Stefan Liute <stefanliute@gmail.com>
2017-03-28 23:20:41 +0100

    Refine API access test

--- Projects/ftheadliner/test/app.test.js
+++ Projects/ftheadliner/test/app.test.js
@@ -16,7 +16,7 @@ describe('Application', function() {
     });
   });
 
-  it ('can use the FT.com Headlines API for search', function(done) {
+  it ('can access the FT.com Headlines API for a simple keyword search', function(done) {
     var postData = JSON.stringify({
       "queryString": "branding"
     });
@@ -28,6 +28,8 @@ describe('Application', function() {
     .send(postData)
     .end(function(err, res) {
       expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body.query.queryString).to.equal('branding');
       done();
     });
   });
