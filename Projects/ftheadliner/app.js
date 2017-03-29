ddf0f4321a6e04942298ff7c331887eacc48a307
Stefan Liute <stefanliute@gmail.com>
2017-03-29 17:43:28 +0100

    Update for usability and readability

--- Projects/ftheadliner/app.js
+++ Projects/ftheadliner/app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/search', search);
+app.use('/', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Projects/ftheadliner/routes/search.js
+++ Projects/ftheadliner/routes/search.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* POST search results page. */
-router.post('/', function(req, res) {
+router.post('/search', function(req, res) {
   res.render('search', { headmast: 'ftheadliner', payload: req.body.searchString });
   console.log('You searched for ' + req.body.searchString);
 });
