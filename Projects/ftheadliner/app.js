aeae859b0573e5e2a6cdb5f0d2e808d10a6d9a10
Stefan Liute <stefanliute@gmail.com>
2017-03-29 16:09:08 +0100

    Use search term for simple POST view (fails)

--- Projects/ftheadliner/app.js
+++ Projects/ftheadliner/app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var search = require('./routes/search');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/search', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ Projects/ftheadliner/routes/search.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* POST search results page. */
+router.post('/search', function(req, res, next) {
+  res.render('search', { headmast: 'ftheadliner', payload: req.body.searchString });
+  console.log('You searched for ' + req.body.searchString);
+});
+
+module.exports = router;
